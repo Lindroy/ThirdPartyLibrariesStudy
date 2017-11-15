d1b8c1100a4b8dee3d4aebface7805cf0701677a
Lindroid <linyulong2014@163.com>
2017-11-15 14:33:56 +0800

    创建项目。

---
+++ app/src/main/java/com/lindroid/thirdpartylibrariesstudy/activity/CatalogActivity.java
@@ -0,0 +1,46 @@
+package com.lindroid.thirdpartylibrariesstudy.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.lindroid.thirdpartylibrariesstudy.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * @author linyulong
+ */
+public class CatalogActivity extends AppCompatActivity {
+
+    @BindView(R.id.list_catalog)
+    ListView listCatalog;
+    private String[] items = {"仿饿了么加入购物车旋转控件", "NumberProgressBar"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catalog);
+        ButterKnife.bind(this);
+        listCatalog.setAdapter(new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,items));
+        listCatalog.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(CatalogActivity.this, "你点击了"+items[position], Toast.LENGTH_SHORT).show();
+                switch (position){
+                    case 0:
+                        break;
+                    case 1:
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/lindroid/thirdpartylibrariesstudy/util/ActivityUtil.java
@@ -0,0 +1,26 @@
+package com.lindroid.thirdpartylibrariesstudy.util;
+
+import android.app.Activity;
+import android.content.Intent;
+
+/**
+ * Created by Lin on 2017/11/15.
+ * Activity工具类
+ */
+
+public class ActivityUtil {
+    //不用finish当前Activity时直接调用此方法
+    public static void startActivity(Activity activity, Class classes){
+        startActivity(activity,classes,false);
+    }
+
+    //函数的重载
+    //需要finish当前Activity时调用此方法，布尔值参数传入true
+    public static void startActivity(Activity activity,Class classes,boolean isFinish){
+        Intent intent = new Intent(activity,classes);
+        activity.startActivity(intent);
+        if (isFinish){
+            activity.finish();
+        }
+    }
+}
