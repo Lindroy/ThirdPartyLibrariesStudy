750f7be19e1c6c606690e482c06c3bb73a9fb405
Lindroid <linyulong2014@163.com>
2018-01-22 14:34:33 +0800

    完成项目所需的MD风格标题栏效果。

--- app/src/main/java/com/lindroid/thirdpartylibrariesstudy/activity/mzbanner/MZBannerActivity.java
+++ app/src/main/java/com/lindroid/thirdpartylibrariesstudy/activity/mzbanner/MZBannerActivity.java
@@ -1,18 +1,58 @@
 package com.lindroid.thirdpartylibrariesstudy.activity.mzbanner;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 import com.lindroid.thirdpartylibrariesstudy.R;
+import com.lindroid.thirdpartylibrariesstudy.util.status.StatusBarHelper;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * @author linyulong
  */
 public class MZBannerActivity extends AppCompatActivity {
 
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.collapse_toolbar)
+    CollapsingToolbarLayout collapseToolbar;
+    @BindView(R.id.app_bar)
+    AppBarLayout appBar;
+    @BindView(R.id.textView2)
+    TextView textView2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mzbanner);
+        ButterKnife.bind(this);
+        StatusBarHelper.setStatusBarLightMode(this);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(false);
+            actionBar.setDisplayShowTitleEnabled(true);
+        }
+
+        Typeface font = Typeface.create("sans-serif-light", Typeface.BOLD);
+        collapseToolbar.setExpandedTitleTypeface(font);
+        textView2.setTypeface(font);
+
+
+//        View titleView = toolbar.getChildAt(0);
+//        titleView.getId();
+//        Toolbar.LayoutParams layoutParams = new Toolbar.LayoutParams
+//                (Toolbar.LayoutParams.WRAP_CONTENT,
+//                        Toolbar.LayoutParams.MATCH_PARENT, Gravity.CENTER);
+//        titleView.setLayoutParams(layoutParams);
+
     }
 }
