288289506c1c12cf60885c3e0740c320c56b5cac
Lindroid <linyulong2014@163.com>
2018-01-30 10:26:22 +0800

    修改MZBannerView的布局，将图片的高度改为一直，并将指示器置于图片底部。

--- app/src/main/java/com/lindroid/thirdpartylibrariesstudy/activity/mzbanner/MZBannerActivity.java
+++ app/src/main/java/com/lindroid/thirdpartylibrariesstudy/activity/mzbanner/MZBannerActivity.java
@@ -1,5 +1,6 @@
 package com.lindroid.thirdpartylibrariesstudy.activity.mzbanner;
 
+import android.content.Context;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
@@ -7,10 +8,20 @@ import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.lindroid.thirdpartylibrariesstudy.R;
 import com.lindroid.thirdpartylibrariesstudy.util.status.StatusBarHelper;
+import com.zhouwei.mzbanner.MZBannerView;
+import com.zhouwei.mzbanner.holder.MZHolderCreator;
+import com.zhouwei.mzbanner.holder.MZViewHolder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -28,12 +39,18 @@ public class MZBannerActivity extends AppCompatActivity {
     AppBarLayout appBar;
     @BindView(R.id.textView2)
     TextView textView2;
+    @BindView(R.id.banner)
+    MZBannerView banner;
+
+    private List<Integer> imageList = new ArrayList<>();
+    private ImageView ivBanner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mzbanner);
         ButterKnife.bind(this);
+        initData();
         StatusBarHelper.setStatusBarLightMode(this);
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
@@ -54,5 +71,63 @@ public class MZBannerActivity extends AppCompatActivity {
 //                        Toolbar.LayoutParams.MATCH_PARENT, Gravity.CENTER);
 //        titleView.setLayoutParams(layoutParams);
 
+        initBanner();
+
+    }
+
+    private void initBanner() {
+        banner.setPages(imageList, new MZHolderCreator() {
+            @Override
+            public MZViewHolder createViewHolder() {
+                return new BannerViewHolder();
+            }
+        });
+    }
+
+    class BannerViewHolder implements MZViewHolder<Integer> {
+
+        @Override
+        public View createView(Context context) {
+            // 返回页面布局
+            View view = LayoutInflater.from(context).inflate(R.layout.item_banner, null);
+            ivBanner = view.findViewById(R.id.banner_image);
+            RelativeLayout rlBanner = view.findViewById(R.id.rl_banner);
+//            int width = UIUtil.getScreenWidth(context)-StatusBarHelper.dp2px(context, 40);
+//            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(width,
+//                    LinearLayout.LayoutParams.MATCH_PARENT);
+            //设置左右边距
+//            params.leftMargin = (int) StatusBarHelper.dp2px(context, 20);
+//            params.rightMargin = (int) StatusBarHelper.dp2px(context, 20);
+//            rlBanner.setLayoutParams(params);
+//            ivBanner.setLayoutParams(params);
+            return view;
+        }
+
+        @Override
+        public void onBind(Context context, int position, Integer data) {
+            ivBanner.setImageResource(data);
+        }
+    }
+
+    private void initData() {
+        imageList.add(R.drawable.cherry);
+        imageList.add(R.drawable.orange);
+        imageList.add(R.drawable.pineapple);
+        imageList.add(R.drawable.cherry);
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        //暂停轮播
+        banner.pause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        //开始轮播
+        banner.start();
     }
 }
---
+++ app/src/main/java/com/lindroid/thirdpartylibrariesstudy/util/UIUtil.java
@@ -0,0 +1,34 @@
+package com.lindroid.thirdpartylibrariesstudy.util;
+
+import android.content.Context;
+import android.util.TypedValue;
+
+/**
+ * @author Lin
+ * @date 2018/1/26
+ * @function
+ */
+
+public class UIUtil {
+    /**
+     * 获取屏幕高度(px)
+     */
+    public static int getScreenHeight(Context context) {
+        return context.getResources().getDisplayMetrics().heightPixels;
+    }
+
+    /**
+     * 获取屏幕宽度(px)
+     */
+    public static int getScreenWidth(Context context) {
+        return context.getResources().getDisplayMetrics().widthPixels;
+    }
+
+    public static float dp2px(Context context, float dp) {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.getResources().getDisplayMetrics());
+    }
+
+    public static float px2dp(Context context, float px) {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX, px, context.getResources().getDisplayMetrics());
+    }
+}
